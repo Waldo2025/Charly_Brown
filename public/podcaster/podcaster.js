76f78ce7bbd1630f4b4d1cdd6d0d3e4ad74f6adc
the-waldus <waldo.lopez85@gmail.com>
2026-06-16 17:32:58 -0500

    Bump podcaster export version

--- public/podcaster/podcaster.js
+++ public/podcaster/podcaster.js
@@ -37,7 +37,7 @@ import {
   continueMontageExportPolling,
   setMontageExportProgress,
   setMontageExportStatus
-} from "./podcaster-montage-export.js?v=2026-06-16.23";
+} from "./podcaster-montage-export.js?v=2026-06-16.24";
 import * as PodcasterResize from "./podcaster-resize.js";
 import { createPodcasterStageFullscreenController } from "./podcaster-fullscreen.js";
 import { createPodcasterMediaReferenceApi } from "./podcaster-media-reference.js?v=2026-05-18.1";
