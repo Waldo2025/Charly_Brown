5fe8f0c4bd61a71a3a06de7d7b89b0b84abf167a
the-waldus <waldo.lopez85@gmail.com>
2026-06-22 13:08:24 -0500

    Aumentar cache-busters a v1.0.10.218 y aplicar fix de red en server.js

--- public/podcaster/podcaster.js
+++ public/podcaster/podcaster.js
@@ -40,7 +40,7 @@ import {
   setMontageExportProgress,
   setMontageExportStatus,
   configureMontageExportRuntime
-} from "./podcaster-montage-export.js?v=2026-06-19.16";
+} from "./podcaster-montage-export.js?v=2026-06-22.1";
 import * as PodcasterResize from "./podcaster-resize.js";
 import { createPodcasterStageFullscreenController } from "./podcaster-fullscreen.js";
 import { createPodcasterMediaReferenceApi } from "./podcaster-media-reference.js?v=2026-05-18.1";
