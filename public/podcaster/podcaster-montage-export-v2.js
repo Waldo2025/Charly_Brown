fa0eec62d47388f1310f95c059d9e742b492d572
the-waldus <waldo.lopez85@gmail.com>
2026-06-28 20:27:45 -0500

    Bump Podcaster export cache version

--- public/podcaster/podcaster-montage-export-v2.js
+++ public/podcaster/podcaster-montage-export-v2.js
@@ -12,7 +12,7 @@ import {
   setMontageExportDownloadButton,
   setMontageExportProgress,
   setMontageExportStatus
-} from "./podcaster-montage-export.js?v=2026-06-29.3";
+} from "./podcaster-montage-export.js?v=2026-06-29.5";
 
 let montageExportV2SubmitLocked = false;
 
--- public/podcaster/podcaster.js
+++ public/podcaster/podcaster.js
@@ -39,11 +39,11 @@ import {
   setMontageExportStatus,
   configureMontageExportRuntime,
   reopenMontageExportModalFromCard
-} from "./podcaster-montage-export.js?v=2026-06-29.3";
+} from "./podcaster-montage-export.js?v=2026-06-29.5";
 import {
   handleMontageExportConfirmClickV2 as handleMontageExportConfirmClick,
   runMontageExportV2 as runMontageExport
-} from "./podcaster-montage-export-v2.js?v=2026-06-29.4";
+} from "./podcaster-montage-export-v2.js?v=2026-06-29.5";
 import * as PodcasterResize from "./podcaster-resize.js";
 import { createPodcasterStageFullscreenController } from "./podcaster-fullscreen.js";
 import { createPodcasterMediaReferenceApi } from "./podcaster-media-reference.js?v=2026-05-18.1";
